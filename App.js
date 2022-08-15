25544656754a8d16976d90fdfefb9a0ded1312ae
전우정 <woojung@jeon-ujeong-ui-MacBookAir.local>
2022-08-16 01:39:23 +0900

    FEAT: Input event, To Do List rendering

--- App.js
+++ App.js
@@ -6,14 +6,29 @@ import {
   View,
   TouchableOpacity,
   TextInput,
+  ScrollView,
 } from "react-native";
 import { theme } from "./colors";
 
 export default function App() {
   const [working, setWorking] = useState(true);
   const [text, setText] = useState("");
+  const [toDos, setToDos] = useState({}); // toDos[Date.now()] = {text: "sth", work: true}
   const travel = () => setWorking(false);
   const work = () => setWorking(true);
+  const addTodo = () => {
+    if (text === "") {
+      return;
+    } else {
+      // const newToDos = Object.assign({}, toDos, {[Date.now()]: {text, work: working}});
+      const newToDos = {
+        ...toDos,
+        [Date.now()]: { text, work: working },
+      };
+      setToDos(newToDos);
+      setText("");
+    }
+  };
 
   return (
     <View style={styles.container}>
@@ -43,8 +58,15 @@ export default function App() {
       <TextInput
         style={styles.input}
         onChangeText={(payload) => setText(payload)}
+        onSubmitEditing={addTodo}
+        value={text}
         placeholder={working ? "Add a To Do" : "Where do you want to go?"}
       ></TextInput>
+      <ScrollView>
+        {Object.keys(toDos).map(key => <View style={styles.toDo} key={key}>
+          <Text style={styles.toDoText}>{toDos[key].text}</Text>
+        </View>)}
+      </ScrollView>
     </View>
   );
 }
@@ -70,7 +92,19 @@ const styles = StyleSheet.create({
     paddingVertical: 15,
     paddingHorizontal: 20,
     borderRadius: 30,
-    marginTop: 20,
+    marginVertical: 20,
     fontSize: 18,
+  },
+  toDo: {
+    backgroundColor: theme.toDoBg,
+    marginBottom: 10,
+    paddingVertical: 20,
+    paddingHorizontal: 30,
+    borderRadius: 30,
+  },
+  toDoText: {
+    color: theme.text,
+    fontSize: 16,
+    fontWeight: '500',
   },
 });
--- colors.js
+++ colors.js
@@ -1,5 +1,6 @@
 export const theme = {
-    bg: 'black',
-    text: 'white',
-    gray: '#696969',
+    bg: "black",
+    text: "white",
+    gray: "#696969",
+    toDoBg: "#5C5C60",
 }
